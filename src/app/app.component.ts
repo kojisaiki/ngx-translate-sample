563c88b60ac183112c037ebc15102c3f1f4ea2a5
Koji Saiki <saikou9901@gmail.com>
2017-06-03 22:52:30 +0900

    アプリで言語を切り替える機能追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -6,19 +6,33 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  private currLang: string;
 
   // メッセージに渡すパラメータ
   param = { value: 'Japan' };
 
-  constructor(translate: TranslateService) {
+  constructor(private translate: TranslateService) {
     // この言語は現在の言語で翻訳が見つからない場合のフォールバックとして使用される
     translate.setDefaultLang('en');
 
     // ブラウザから（Accept-Languageヘッダから？）言語を取得する
     const browserLang: string = translate.getBrowserLang();
     // useで使用する言語を指定する。指定された言語が使用できない場合、ローダによって処理される（ここではフォールバック先があるため、それが使用される）
-    translate.use(browserLang.match(/ja|en/) ? browserLang : 'en');
+    this.currLang = browserLang.match(/ja|en/) ? browserLang : 'en';
+    translate.use(this.currLang);
+  }
+
+  useEn() {
+    this.currLang = 'en';
+    this.translate.use(this.currLang);
+  }
+
+  useJa() {
+    this.currLang = 'ja';
+    this.translate.use(this.currLang);
+  }
   }
 }
