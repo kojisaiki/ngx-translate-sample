7f4715b67d3627048300174e93bf794cf46e1cf0
Koji Saiki <saikou9901@gmail.com>
2017-06-03 19:50:22 +0900

    ブラウザから指定された言語で表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,12 @@ export class AppComponent {
   param = { value: 'Japan' };
 
   constructor(translate: TranslateService) {
-    // この言語は現在の言語で翻訳が見つからない場合のフォールバックとして使用されます
+    // この言語は現在の言語で翻訳が見つからない場合のフォールバックとして使用される
     translate.setDefaultLang('en');
 
-    // 使用する言語です。指定された言語が使用できない場合、ローダによって処理される（ここではフォールバック先があるため、それが使用される）
-    translate.use('en');
+    // ブラウザから（Accept-Languageヘッダから？）言語を取得する
+    let browserLang: string = translate.getBrowserLang();
+    // useで使用する言語を指定する。指定された言語が使用できない場合、ローダによって処理される（ここではフォールバック先があるため、それが使用される）
+    translate.use(browserLang.match(/ja|en/) ? browserLang : 'en');
   }
 }
